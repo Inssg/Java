933a4198a58961f83ea49782b0c84653d4cbfb6a
Inssg <rnlrhdwk98@gmail.com>
2022-05-10 00:05:59 +0900

    Javabasic-1

--- Test/src/ArithmeticOperator/Arithmetic.java
+++ Test/src/ArithmeticOperator/Arithmetic.java
@@ -1,5 +1,16 @@
 package ArithmeticOperator;
 
 public class Arithmetic {
-
+	public static void main(String[] args) {
+	int value3 = 3;
+	int value4 = value3++;
+	System.out.println(value3);
+	System.out.println(value4);
+	System.out.println();
+	
+	int value5 = 3;
+	int value6 = ++value5;
+	System.out.println(value5);
+	System.out.println(value6);
+	}
 }
